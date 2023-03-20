d3f1d01be19a479139755cdf925edc7c634c9bc3
AlekseyBabich <babichalex1986@gmail.com>
2023-03-20 01:41:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+    const [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('')
+
+    function increment() {
+        setLikes(likes + 1)
+    }
+
+    function decrement() {
+        setLikes(likes - 1)
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{likes}</h1>
+        <h1>{value}</h1>
+        <input onChange={event => setValue(event.target.value)}/>
+        <button onClick={increment}>Увеличиить</button>
+        <button onClick={decrement}>Уменьшить</button>
     </div>
   );
 }
