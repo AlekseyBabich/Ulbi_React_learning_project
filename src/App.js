664b2be772d4e882ecb1f1f60a16cc521d0389a6
AlekseyBabich <babichalex1986@gmail.com>
2023-03-21 11:46:38 +0600

    create custom hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css'
 import PostList from './Components/PostList';
-import PostForm from "./Components/PostForm";
-import PostFilter from "./Components/PostFilter";
-import MyModal from "./Components/UI/modal/MyModal";
-import MyButton from "./Components/UI/button/MyButton";
+import PostForm from './Components/PostForm';
+import PostFilter from './Components/PostFilter';
+import MyModal from './Components/UI/modal/MyModal';
+import MyButton from './Components/UI/button/MyButton';
+import { usePosts } from './Components/hooks/usePosts';
 
 function App() {
-  const [ posts, setPosts ] = useState([
-    { id: 1, title: 'аа', body: 'бб' },
-    { id: 2, title: 'гг', body: 'аа' },
-    { id: 3, title: 'вв', body: 'яя' }
-  ])
-
+  const [ posts, setPosts ] = useState([])
   const [ filter, setFilter ] = useState({ sort: '', query: '' })
-  const [modal, setModal] = useState(false)
-
-  const sortedPost = useMemo(() => {
-    console.log('ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТС')
-    if (filter.sort) {
-      return [ ...posts ].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [ filter.sort, posts ])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [ filter.query, sortedPost ])
+  const [ modal, setModal ] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([ ...posts, newPost ])
@@ -39,10 +24,10 @@ function App() {
 
   return (
     <div className='App'>
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+      <MyButton style={ { marginTop: 30 } } onClick={ () => setModal(true) }>
         Создать пост
       </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
+      <MyModal visible={ modal } setVisible={ setModal }>
         <PostForm create={ createPost }/>
       </MyModal>
       <hr style={ { margin: '15px 0' } }/>
