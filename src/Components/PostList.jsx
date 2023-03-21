856a4f179778c2c2cb72ac9d8085525422d01e65
AlekseyBabich <babichalex1986@gmail.com>
2023-03-21 11:26:21 +0600

    install react-transition-group

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PostItem from './PostItem';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const PostList = ({ posts, title, remove }) => {
 
-  if(!posts.length) {
+  if (!posts.length) {
     return (
-     <h1 style={ { textAlign: 'center' } }>
-       Посты не найдены!
-     </h1>
+      <h1 style={ { textAlign: 'center' } }>
+        Посты не найдены!
+      </h1>
     )
   }
 
@@ -16,9 +17,17 @@ const PostList = ({ posts, title, remove }) => {
       <h1 style={ { textAlign: 'center' } }>
         { title }
       </h1>
-      { posts.map((post, index) =>
-        <PostItem remove={remove} number={ index + 1 } post={ post } key={ post.id }/>
-      ) }
+      <TransitionGroup>
+        { posts.map((post, index) =>
+          <CSSTransition
+            key={ post.id }
+            timeout={ 500 }
+            classNames='post'
+          >
+            <PostItem remove={ remove } number={ index + 1 } post={ post } />
+          </CSSTransition>
+        ) }
+      </TransitionGroup>
     </div>
   );
 };
