73e4cb96dc5e53a2f0dc1629714e1d4ae5fa6a8e
AlekseyBabich <babichalex1986@gmail.com>
2023-03-22 13:07:44 +0600

    add axios query create loader component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css'
 import PostList from './Components/PostList';
 import PostForm from './Components/PostForm';
@@ -6,12 +6,29 @@ import PostFilter from './Components/PostFilter';
 import MyModal from './Components/UI/modal/MyModal';
 import MyButton from './Components/UI/button/MyButton';
 import { usePosts } from './Components/hooks/usePosts';
+import axios from "axios";
+import PostService from "./Api/PostService";
+import Loader from "./Components/UI/loader/Loader";
 
 function App() {
   const [ posts, setPosts ] = useState([])
   const [ filter, setFilter ] = useState({ sort: '', query: '' })
   const [ modal, setModal ] = useState(false)
+  const [ isPostsLoading, setIsPostsLoading ] = useState(false)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  async function fetchPosts() {
+    setIsPostsLoading(true)
+    setTimeout(async () => {
+      const posts = await PostService.getAll()
+      setPosts(posts)
+      setIsPostsLoading(false)
+    }, 1000)
+  }
 
   const createPost = (newPost) => {
     setPosts([ ...posts, newPost ])
@@ -32,7 +49,10 @@ function App() {
       </MyModal>
       <hr style={ { margin: '15px 0' } }/>
       <PostFilter filter={ filter } setFilter={ setFilter }/>
-      <PostList remove={ removePost } posts={ sortedAndSearchedPosts } title='Посты про JS'/>
+      {isPostsLoading
+        ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+        : <PostList remove={ removePost } posts={ sortedAndSearchedPosts } title='Посты про JS'/>
+      }
     </div>
   );
 }
