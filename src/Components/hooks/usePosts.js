9d0a2de2394b8b1841b9b85038870394d50755b7
AlekseyBabich <babichalex1986@gmail.com>
2023-03-22 22:35:51 +0600

    Pagination

--- src/Components/hooks/usePosts.js
+++ src/Components/hooks/usePosts.js
@@ -2,7 +2,6 @@ import { useMemo } from 'react';
 
 export const useSortedPost = (posts, sort) => {
   const sortedPost = useMemo(() => {
-    console.log('ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТС')
     if (sort) {
       return [ ...posts ].sort((a, b) => a[sort].localeCompare(b[sort]))
     }
