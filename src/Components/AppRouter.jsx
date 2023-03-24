1abc76e25a75f6eebf8c467abe64d72a103b4ac3
AlekseyBabich <babichalex1986@gmail.com>
2023-03-24 14:45:50 +0600

    add login form and isAuth

--- src/Components/AppRouter.jsx
+++ src/Components/AppRouter.jsx
@@ -4,16 +4,28 @@ import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostIdPage from "../pages/PostIdPage";
+import Login from "../pages/login";
 
 const AppRouter = () => {
+
+  const isAuth = true
+
   return (
-    <Routes>
-      <Route path='/about' element={ <About/> }/>
-      <Route path='/posts' element={ <Posts/> }/>
-      <Route path='/posts/:id' element={ <PostIdPage/> }/>
-      <Route path='/error' element={ <Error/> }/>
-      <Route path='/*' element={ <Navigate to='/error' replace/> }/>
-    </Routes>
+    isAuth
+      ?
+      <Routes>
+        <Route path='/about' element={ <About/> }/>
+        <Route path='/posts' element={ <Posts/> }/>
+        <Route path='/posts/:id' element={ <PostIdPage/> }/>
+        <Route path='/error' element={ <Error/> }/>
+        <Route path='/*' element={ <Navigate to='/posts' replace/> }/>
+      </Routes>
+      :
+      <Routes>
+        <Route path='/login' element={ <Login/> } />
+        <Route path='/*' element={ <Navigate to='/login' replace/> }/>
+      </Routes>
+
   );
 };
 
